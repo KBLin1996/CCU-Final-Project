f6843d4955abeca2d7807fa581e3c58e08eeba57
KB <popotwtw@gmail.com>
2018-12-27 02:12:39 +0800

    Update models.py

--- models.py
+++ models.py
@@ -21,10 +21,12 @@ def RandomWeightedAverage():
         return (weights * input1) + ((1 - weights) * input2)
     return Lambda(block)
 
+'''
 def set_trainable(model, trainable):
     for layer in model.layers:
         layer.trainable=trainable
     model.trainable=trainable
+'''
 
 def gradient_penalty_loss(y_pred, averaged_samples, gradient_penalty_weight):
     """Calculates the gradient penalty loss for a batch of "averaged" samples.
